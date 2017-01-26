50d7b64d60768a76168e8b9d62b77f7382d74560
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-26 12:32:29 +1100

    closer

--- src/index.d.ts
+++ src/index.d.ts
@@ -45,6 +45,10 @@ interface ComponentLifecycle<P> {
   componentWillUnmount?(): void;
 }
 
+interface Attributes<T> {
+  key?: Key;
+  ref?: (t: T) => any;
+}
 
 // ----------------------------------------------------------------------
 // Event System
@@ -110,6 +114,158 @@ export interface DOMAttributes<T> {
   onPasteCapture?: ClipboardEventHandler<T>;
 }
 
+interface HTMLAttributes<T> extends DOMAttributes<T> {
+  // Standard HTML Attributes
+  accept?: string;
+  acceptCharset?: string;
+  accessKey?: string;
+  action?: string;
+  allowFullScreen?: boolean;
+  allowTransparency?: boolean;
+  alt?: string;
+  async?: boolean;
+  autoComplete?: string;
+  autoFocus?: boolean;
+  autoPlay?: boolean;
+  capture?: boolean;
+  cellPadding?: number | string;
+  cellSpacing?: number | string;
+  charSet?: string;
+  challenge?: string;
+  checked?: boolean;
+  classID?: string;
+  className?: string;
+  cols?: number;
+  colSpan?: number;
+  content?: string;
+  contentEditable?: boolean;
+  contextMenu?: string;
+  controls?: boolean;
+  coords?: string;
+  crossOrigin?: string;
+  data?: string;
+  dateTime?: string;
+  default?: boolean;
+  defer?: boolean;
+  dir?: string;
+  disabled?: boolean;
+  download?: any;
+  draggable?: boolean;
+  encType?: string;
+  form?: string;
+  formAction?: string;
+  formEncType?: string;
+  formMethod?: string;
+  formNoValidate?: boolean;
+  formTarget?: string;
+  frameBorder?: number | string;
+  headers?: string;
+  height?: number | string;
+  hidden?: boolean;
+  high?: number;
+  href?: string;
+  hrefLang?: string;
+  htmlFor?: string;
+  httpEquiv?: string;
+  id?: string;
+  inputMode?: string;
+  integrity?: string;
+  is?: string;
+  keyParams?: string;
+  keyType?: string;
+  kind?: string;
+  label?: string;
+  lang?: string;
+  list?: string;
+  loop?: boolean;
+  low?: number;
+  manifest?: string;
+  marginHeight?: number;
+  marginWidth?: number;
+  max?: number | string;
+  maxLength?: number;
+  media?: string;
+  mediaGroup?: string;
+  method?: string;
+  min?: number | string;
+  minLength?: number;
+  multiple?: boolean;
+  muted?: boolean;
+  name?: string;
+  nonce?: string;
+  noValidate?: boolean;
+  open?: boolean;
+  optimum?: number;
+  pattern?: string;
+  placeholder?: string;
+  playsInline?: boolean;
+  poster?: string;
+  preload?: string;
+  radioGroup?: string;
+  readOnly?: boolean;
+  rel?: string;
+  required?: boolean;
+  reversed?: boolean;
+  role?: string;
+  rows?: number;
+  rowSpan?: number;
+  sandbox?: string;
+  scope?: string;
+  scoped?: boolean;
+  scrolling?: string;
+  seamless?: boolean;
+  selected?: boolean;
+  shape?: string;
+  size?: number;
+  sizes?: string;
+  span?: number;
+  spellCheck?: boolean;
+  src?: string;
+  srcDoc?: string;
+  srcLang?: string;
+  srcSet?: string;
+  start?: number;
+  step?: number | string;
+  style?: any;
+  summary?: string;
+  tabIndex?: number;
+  target?: string;
+  title?: string;
+  type?: string;
+  useMap?: string;
+  value?: string | string[] | number;
+  width?: number | string;
+  wmode?: string;
+  wrap?: string;
+
+  // RDFa Attributes
+  about?: string;
+  datatype?: string;
+  inlist?: any;
+  prefix?: string;
+  property?: string;
+  resource?: string;
+  typeof?: string;
+  vocab?: string;
+
+  // Non-standard Attributes
+  autoCapitalize?: string;
+  autoCorrect?: string;
+  autoSave?: string;
+  color?: string;
+  itemProp?: string;
+  itemScope?: boolean;
+  itemType?: string;
+  itemID?: string;
+  itemRef?: string;
+  results?: number;
+  security?: string;
+  unselectable?: boolean;
+}
+
+interface HTMLProps<T> extends HTMLAttributes<T>, Attributes<T> {
+}
+
 /** DOM element is what createElement returns */
 export interface DOMElement<P extends DOMAttributes<T>, T extends Element> extends ThElement<P> {
   type: string;
@@ -130,5 +286,11 @@ export function createElement(node: any): DOMElement<any, any>;
 declare global {
   namespace JSX {
     interface Element extends ThElement<any> { }
+    interface ElementAttributesProperty { props: {}; }
+    interface IntrinsicAttributes extends Attributes<void> { }
+    interface IntrinsicClassAttributes<T> extends Attributes<T> { }
+    interface InstrinsicElements {
+      div: HTMLProps<HTMLDivElement>;
+    }
   }
 }
---
+++ src/test/sample.tsx
@@ -0,0 +1,8 @@
+/**
+ * Used as a sample what the code can look like.
+ * It will not work without us actually writing the code
+ */
+import * as Th from '../index';
+
+
+<div>Hello world</div>
