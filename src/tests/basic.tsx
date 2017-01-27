b1df213bb31053b22c7927559c5bd2cb3d3fc14a
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-27 12:26:10 +1100

    add tests

--- src/tests/basic.tsx
+++ src/tests/basic.tsx
@@ -2,7 +2,7 @@
  * Used as a sample what the code can look like.
  * It will not work without us actually writing the code
  */
-import * as Th from '../index'
+import * as TH from '../index'
 
 /** Create component as a function */
 const FunctionExample = ({ text }: { text: string }) => <div>{text}</div>
@@ -20,3 +20,10 @@ let functionExample = <FunctionExample text={"Hello world"} />
 
 /** Use a class */
 let classExample = <ClassExample text={"Hello world"} />
+
+import * as assert from 'assert';
+describe('Server rendering', () => {
+  it('should render to string', () => {
+    assert.equal(TH.renderToString(functionExample), '<div>Hello world</div>');
+  })
+});
