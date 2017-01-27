1824876dbf269141f17f84d22f1a65b16197e475
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-27 13:25:03 +1100

    closer

--- src/tests/karma/tests/basic.tsx
+++ src/tests/karma/tests/basic.tsx
@@ -1,6 +1,15 @@
 import * as assert from 'assert';
-describe('Browser testing', () => {
-  it('should pass', () => {
+import { innerHTML } from '../../../tools/utils';
+import * as TH from '../../../index';
+
+describe('createElement', () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  it('should create an element', () => {
+    const node = <a />;
   });
   it('should pass2', () => {
     assert(true);
