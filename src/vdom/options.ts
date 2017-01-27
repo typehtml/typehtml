3f95b8dbdc90c370f009794439c221e015ef682f
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-28 04:21:52 +1100

    add types :rose:

--- src/vdom/options.ts
+++ src/vdom/options.ts
@@ -1,11 +1,14 @@
+import { VNode } from '../types';
+export type Hook<T> = (item: T) => any | null;
+
 export default {
   recyclingEnabled: true,
 
   /** Called on new VNode creation */
-  createVNode: null,
+  createVNode: null as Hook<VNode<any>>,
 
   /** Other life cycle hooks */
-	beforeRender: null,
+  beforeRender: null,
 	afterRender: null,
 	afterMount: null,
 	afterUpdate: null,
