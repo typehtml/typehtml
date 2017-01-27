1e8431adc7718bea8b89390804348411684778a8
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-27 13:13:00 +1100

    working on karma

---
+++ src/tests/karma/karma.config.ts
@@ -0,0 +1,76 @@
+import path = require('path');
+
+export = function(config) {
+  config.set({
+    basePath: path.join(__dirname, '..', '..'),
+    browsers: [
+      'Chrome'
+    ],
+    preprocessors: {
+      'src/**/*': ['webpack']
+    },
+    webpack: {
+      module: {
+        loaders: [
+          {
+            test: /\.tsx?$/,
+            loaders: ['ts-loader'],
+            exclude: /node_modules/
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+      performance: {
+        hints: false
+      }
+    },
+    webpackMiddleware: {
+      stats: 'errors-only',
+      noInfo: true
+    },
+    concurrency: 1,
+    browserConsoleLogOptions: {
+      terminal: true
+    },
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 2,
+    browserLogOptions: {
+      terminal: true
+    },
+    browserNoActivityTimeout: 2 * 60 * 1000,
+    captureTimeout: 2 * 60 * 10000,
+    autoWatch: false,
+    singleRun: true
+  });
+
+  config.set({
+    frameworks: [
+      'mocha'
+    ],
+    files: [
+      'lib/tests/karma/tests/**/*.js',
+    ],
+    reporters: [
+      'failed'
+    ]
+  });
+
+  const ci = String(process.env.CI).match(/^(1|true)$/gi);
+  if (ci) {
+    const travisLaunchers = {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    };
+    config.set({
+      customLaunchers: travisLaunchers,
+      browsers: [
+        'Firefox',
+        'Chrome_travis_ci'
+      ]
+    });
+  }
+};
---
+++ src/tests/karma/tests/basic.tsx
@@ -0,0 +1,5 @@
+import * as assert from 'assert';
+describe('Browser testing', () => {
+  it('should pass', () => {
+  });
+})
