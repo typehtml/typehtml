886c21ac92e8c8347e5af533cd1291fddb7eb8d2
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-28 18:13:21 +1100

    createElement

--- src/dom/createElement.ts
+++ src/dom/createElement.ts
@@ -1,15 +1,22 @@
 import * as types from '../types';
 
 
-export declare function createElement<P extends types.HTMLAttributes<T>, T extends Element>(
+export function createElement<P extends types.HTMLAttributes<T>, T extends Element>(
   name: string,
   props?: types.ThAttributes<T> & P,
   ...children: types.ThChildren[]): types.DOMElement<T>;
-export declare function createElement<P>(
+export function createElement<P>(
   name: types.ComponentFunction<P>,
   props?: types.ThAttributes<P> & P,
   ...children: types.ThChildren[]): types.ComponentFunctionElement<P>;
-export declare function createElement<P>(
+export function createElement<P>(
   name: types.ComponentClass<P>,
   props?: types.ThAttributes<P> & P,
   ...children: types.ThChildren[]): types.ComponentClassElement<P>;
+export function createElement(
+  type: any,
+  props?: any,
+  ...children: any[]
+): any {
+
+}
