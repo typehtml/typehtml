525034f083bd37452c6be90c55748be66cd6287b
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-29 12:44:55 +1100

    take props

--- src/tests/testem/componentFunction.tsx
+++ src/tests/testem/componentFunction.tsx
@@ -13,6 +13,12 @@ describe('component function', () => {
     assert.equal(elm.innerHTML, '<div>Hello World</div>');
   });
 
+  it('can take props', () => {
+    const Sample = ({ text }) => <div>{text}</div>;
+    Th.render(<Sample text="Hello World"/>, elm);
+    assert.equal(elm.innerHTML, '<div>Hello World</div>');
+  });
+
   it('can render children', () => {
     const Sample = ({ children }: { children?}) => <div>{children}</div>;
     Th.render(<Sample>Hello World</Sample>, elm);
