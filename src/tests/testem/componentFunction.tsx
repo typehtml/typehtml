a45a417ad8deb2ff118867294ed4c98c8e065e7b
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-30 09:46:16 +1100

    reactive component function test

--- src/tests/testem/componentFunction.tsx
+++ src/tests/testem/componentFunction.tsx
@@ -1,5 +1,6 @@
 import * as Th from '../../index';
 import * as assert from 'assert';
+import { observable, action } from 'mobx';
 
 describe('component function', () => {
   let elm: HTMLDivElement;
@@ -23,5 +24,30 @@ describe('component function', () => {
     const Sample = ({ children }: { children?}) => <div>{children}</div>;
     Th.render(() => <Sample>Hello World</Sample>, elm);
     assert.equal(elm.innerHTML, '<div>Hello World</div>');
+  });
+})
+
+
+describe('component function reactive', () => {
+  let elm: HTMLDivElement;
+  beforeEach(() => {
+    elm = document.createElement('div');
+  });
+
+  class State {
+    @observable foo = 123;
+    @action changeFoo() {
+      this.foo = 456;
+    }
+  }
+  let state = new State();
+  beforeEach(() => state = new State());
+
+  it('can re-render a component', () => {
+    const Sample = ({ }) => <div>{state.foo}</div>;
+    Th.render(() => <Sample />, elm);
+    assert.equal(elm.innerHTML, '<div>123</div>');
+    state.changeFoo();
+    assert.equal(elm.innerHTML, '<div>456</div>');
   });
 })
