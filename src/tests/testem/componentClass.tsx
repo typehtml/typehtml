496853b8c076a86fc82e4488aedd229550aed1dc
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-29 13:43:04 +1100

    can take children

--- src/tests/testem/componentClass.tsx
+++ src/tests/testem/componentClass.tsx
@@ -27,4 +27,15 @@ describe('component class', () => {
     Th.render(<Sample text="Hello World" />, elm);
     assert.equal(elm.innerHTML, '<div>Hello World</div>');
   });
+
+  it('can take children', () => {
+    class Sample {
+      constructor(public props: { children?: any }) { }
+      render() {
+        return <div>{this.props.children}</div>;
+      }
+    }
+    Th.render(<Sample>Hello World</Sample>, elm);
+    assert.equal(elm.innerHTML, '<div>Hello World</div>');
+  });
 });
